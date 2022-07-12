776715d42e0cd481495e03e63eb6967f62a40a5b
TommyZ2197 <tz2197@stern.nyu.edu>
2022-07-12 19:54:20 -0400

    user input

--- game.py
+++ game.py
@@ -5,3 +5,7 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+print("Welcome to Rock Paper Scissors")
+user_choice = input("Please make a selection ('Rock', 'Paper', 'Scissors'):")
+print(f"You chose:, '{user_choice}' ")
+
