7c5516895cd1be70481ea040a6eb4e79420ce986
TommyZ2197 <tz2197@stern.nyu.edu>
2022-07-13 20:15:34 -0400

    Determine the winner my way

--- game.py
+++ game.py
@@ -6,11 +6,36 @@ import random
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #USER INPUTS
-print("Welcome to Rock Paper Scissors")
-user_choice = input("Please make a selection ('Rock', 'Paper', 'Scissors'):")
-print(f"You chose:, '{user_choice}' ")
+print("Welcome Player One to Rock Paper Scissors")
+player_choice = input("Please make a selection ('Rock', 'Paper', 'Scissors'):")
+player_choice = player_choice.lower()
 
+print(f"You chose:, '{player_choice}' ")
+
+#Validate user inputs
+valid_options = ["rock", "paper", "scissors"]
+if player_choice not in valid_options
+    print("Sorry, please come back and try again")
+    exit ()
+else continue 
 #computer choice
 valid_options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(valid_options)
 print("Computer chose:", computer_choice)
+
+#Determine the winner
+winner: {'rock:scissors', 'paper:rock', 'scissors:paper'} 
+if player_choice == computer_choice
+    print("TIE")
+elif player_choice == winner[computer_choice]
+    print ("Amazing, you won!")
+elif computer_choice == winner[player_choice]
+    print("Computer wins :( ")
+
+print("Thank you for playing! See you soon!)")
+
+
+
+
+
+
